e6f52bce4fe1a982233d4d8b4b550f46f4b2684d
Shusei Matoike <smat1957@gmail.com>
2025-08-02 00:59:28 +0900

    debug

--- json2tex4EWord/do0 2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/usr/bin/python3 /Users/mat/Documents/PycharmProjects/json2tex4EWord/main.py "$1" "$2"
-sh /Users/mat/Documents/PycharmProjects/json2tex4EWord/do1.sh "$1"
-sh /Users/mat/Documents/PycharmProjects/json2tex4EWord/do2.sh "$1"
--- json2tex4EWord/do1 2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /Users/mat/Documents/PycharmProjects/json2tex4EWord
-/usr/local/texlive/2024/bin/universal-darwin/uplatex "$1".tex
-/usr/local/texlive/2024/bin/universal-darwin/dvipdfm "$1"
